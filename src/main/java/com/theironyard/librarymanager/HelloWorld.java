f82ac9d15330c504ec315b25881fcd72d5e390f5
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-17 23:39:21 -0500

    Demonstrate getting a bean from the ApplicationContext

---
+++ src/main/java/com/theironyard/librarymanager/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.theironyard.librarymanager;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelloWorld {
+    void sayHello() {
+        System.out.println("Hello world!");
+    }
+}
--- src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
@@ -2,11 +2,15 @@ package com.theironyard.librarymanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class LibraryManagerApplication {
+    private static ConfigurableApplicationContext ctx;
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        ctx = SpringApplication.run(LibraryManagerApplication.class, args);
+        HelloWorld hello = (HelloWorld) ctx.getBean("helloWorld");
+        hello.sayHello();
+    }
 }
