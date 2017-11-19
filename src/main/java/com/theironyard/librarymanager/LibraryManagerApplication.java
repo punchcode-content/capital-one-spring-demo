13a118f3de74109c02e965da32240990e01c4201
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-18 21:17:48 -0500

    Print list of all available beans

--- src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
@@ -3,12 +3,21 @@ package com.theironyard.librarymanager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import us.dreisbach.NameService;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class LibraryManagerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LibraryManagerApplication.class, args);
+        ConfigurableApplicationContext ctx = SpringApplication.run(LibraryManagerApplication.class, args);
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
+        System.out.println(beanNames.length + " beans");
     }
 
     @Autowired
