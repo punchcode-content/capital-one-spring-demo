f4d9cde68b22341fcffcb28333f4b48749c738c4
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-18 23:07:10 -0500

    Add Author and AuthorService

---
+++ src/main/java/com/theironyard/librarymanager/services/AuthorService.java
@@ -0,0 +1,9 @@
+package com.theironyard.librarymanager.services;
+
+import com.theironyard.librarymanager.entities.Author;
+
+import java.util.List;
+
+public interface AuthorService {
+    List<Author> listAllAuthors();
+}
---
+++ src/main/java/com/theironyard/librarymanager/services/MapAuthorService.java
@@ -0,0 +1,31 @@
+package com.theironyard.librarymanager.services;
+
+import com.theironyard.librarymanager.entities.Author;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapAuthorService implements AuthorService {
+    private Map<Integer, Author> authors;
+
+    public MapAuthorService() {
+        authors = new HashMap<>();
+    }
+
+    @Override
+    public List<Author> listAllAuthors() {
+        return null;
+    }
+
+    private void createSampleAuthors() {
+        Author author1 = new Author(1, "Kathy Sierra");
+        authors.put(1, author1);
+
+        Author author2 = new Author(2, "James Gosling");
+        authors.put(2, author2);
+
+        Author author3 = new Author(3, "Joshua Bloch");
+        authors.put(3, author3);
+    }
+}
