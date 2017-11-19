a2789403aedd502d691676cdb1d5d517408b511b
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-18 21:21:37 -0500

    Remove demonstration junk

--- src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
@@ -1,27 +1,11 @@
 package com.theironyard.librarymanager;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import us.dreisbach.NameService;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class LibraryManagerApplication {
     public static void main(String[] args) {
-        ConfigurableApplicationContext ctx = SpringApplication.run(LibraryManagerApplication.class, args);
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
-        System.out.println(beanNames.length + " beans");
-    }
-
-    @Autowired
-    public LibraryManagerApplication(NameService nameService) {
-        System.out.println("Hi, my name is " + nameService.getName());
+        SpringApplication.run(LibraryManagerApplication.class, args);
     }
 }
--- src/main/java/com/theironyard/librarymanager/com/theironyard/librarymanager/config/BeanConfig.java
+++ src/main/java/com/theironyard/librarymanager/com/theironyard/librarymanager/config/BeanConfig.java
@@ -1,13 +1,7 @@
 package com.theironyard.librarymanager.com.theironyard.librarymanager.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import us.dreisbach.NameService;
 
 @Configuration
 public class BeanConfig {
-    @Bean
-    public NameService nameService() {
-        return new NameService();
-    }
 }
