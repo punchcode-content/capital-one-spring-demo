382617b8383229475704f97089b34e31c1f3afbf
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-18 20:44:41 -0500

    Demonstrate use of @ComponentScan
    The NameService is in the us.dreisbach package. Without @ComponentScan,
    it's not registered as a bean.

--- src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
@@ -3,29 +3,18 @@ package com.theironyard.librarymanager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
+import org.springframework.context.annotation.ComponentScan;
+import us.dreisbach.NameService;
 
 @SpringBootApplication
+@ComponentScan("us.dreisbach")
 public class LibraryManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagerApplication.class, args);
     }
 
     @Autowired
-    public LibraryManagerApplication(HelloWorld _hello) {
-        _hello.sayHello();
-    }
-
-    private HelloWorld hello;
-
-    @Autowired
-    public void setHello(HelloWorld hello) {
-        this.hello = hello;
-    }
-
-    @PostConstruct
-    public void init() {
-        hello.sayHello();
+    public LibraryManagerApplication(NameService nameService) {
+        System.out.println("Hi, my name is " + nameService.getName());
     }
 }
---
+++ src/main/java/us/dreisbach/NameService.java
@@ -0,0 +1,10 @@
+package us.dreisbach;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NameService {
+    public String getName() {
+        return "Clinton Dreisbach";
+    }
+}
