1a295bd2ec6da0a8dede78e819906c99cc2cdf5f
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-18 20:48:23 -0500

    Demonstrate use of @Config
    BeanConfig gives us a bean to access. Without it, and without
    @ComponentScan, this code would not work.

--- src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
+++ src/main/java/com/theironyard/librarymanager/LibraryManagerApplication.java
@@ -3,11 +3,9 @@ package com.theironyard.librarymanager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import us.dreisbach.NameService;
 
 @SpringBootApplication
-@ComponentScan("us.dreisbach")
 public class LibraryManagerApplication {
     public static void main(String[] args) {
         SpringApplication.run(LibraryManagerApplication.class, args);
