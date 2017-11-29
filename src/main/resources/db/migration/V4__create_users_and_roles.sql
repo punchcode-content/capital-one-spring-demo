4922cbb2217d7faa8e62a98875ff78117491fbfa
Clinton Dreisbach <clinton@dreisbach.us>
2017-11-29 10:20:25 -0500

    Added Spring Security starter

---
+++ src/main/resources/db/migration/V4__create_users_and_roles.sql
@@ -0,0 +1,18 @@
+CREATE TABLE users (
+  id                 SERIAL PRIMARY KEY,
+  username           VARCHAR(255) NOT NULL UNIQUE,
+  encrypted_password VARCHAR(255)
+);
+
+CREATE TABLE roles (
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE roles_users (
+  user_id INTEGER NOT NULL,
+  role_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (role_id) REFERENCES roles (id),
+  PRIMARY KEY (user_id, role_id)
+);
